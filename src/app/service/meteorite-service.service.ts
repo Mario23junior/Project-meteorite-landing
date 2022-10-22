0869fe50bf166a1c5b644ff793a14777aa07d578
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-22 19:23:52 -0300

    Criando service de listar todos os meteoritos.

--- src/app/service/meteorite-service.service.ts
+++ src/app/service/meteorite-service.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Meteorite } from '../components/model/Meteorite.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeteoriteServiceService {
 
-  constructor() { }
+  public baseUrl = "http://localhost:8080";
+  public uri = "/api/v1/meteorite/";
+
+  constructor(private httpClient: HttpClient) { }
+
+  listAll(): Observable<Meteorite[]> {
+    let url = this.baseUrl+this.uri
+    let request = this.httpClient.get<Meteorite[]>(url);
+    return request
+  }
 }
